5041d193022be8a8ad9aff928e2c0abc73c9ec15
slee713 <steven.lee1393@gmail.com>
2020-08-27 15:07:24 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -12,4 +12,82 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+  url = "http://localhost:3000/toys/"
+  const addToyForm = document.querySelector("form.add-toy-form");
+  const div = document.querySelector("div#toy-collection");
+
+  // Iterate through toys from url and add to div container using Add AToy func
+  function addToys(toys) {
+    toys.forEach( toy => addAToy(toy))
+  }
+
+  // add a specific toy to the container 
+  function addAToy(toy) {
+    let card = document.createElement('div')
+    card.setAttribute("class", "card")
+    let h2 = document.createElement("h2")
+    let img = document.createElement("img")
+    let p = document.createElement("p")
+    let btn = document.createElement("button")
+    h2.innerText = toy.name
+    img.src = toy.image
+    img.setAttribute("class", "toy-avatar")
+    p.innerText = `${toy.likes} Likes`
+    btn.setAttribute("class", "like-btn")
+    btn.innerText = "Like <3"
+    card.append(h2, img, p, btn)
+    div.append(card)
+
+    // Create Event listener for each button created for each toy
+    btn.addEventListener("click", () => {
+      p.innerText = `${toy.likes + 1} Likes`
+      config = {
+        method: "PATCH",
+        headers: {
+          "Content-Type": 'application/json',
+          Accept: 'application/json'
+        },
+        body: JSON.stringify({
+          likes: toy.likes + 1
+        } )
+      }
+      fetch(url+toy.id, config)
+      .then(resp => resp.json())
+
+    })
+
+
+
+  }
+
+  fetch(url)
+  .then(resp => resp.json())
+  .then(toys => addToys(toys))
+
+  addToyForm.addEventListener("submit", () => {
+    let name = event.target[0].value;
+    let image = event.target[1].value;
+
+    const config = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        name: name,
+        image: image,
+        likes: 0
+      })
+    }
+
+    fetch(url, config)
+    .then(resp => resp.json())
+    
+
+  })
+
+
+
 });
